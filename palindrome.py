7ca66f72f9b6fbd8e3a5af2dc6ee0eb98bedbae4
cindy kivala <cindykivala@gmail.com>
2025-08-09 21:36:15 +0300

    Exlamation and spaces ignore logic

--- palindrome.py
+++ palindrome.py
@@ -4,19 +4,32 @@
 #Our code should overlook punct. and whittespaces and focus on letters
 
 def choose_palindromes(words):
-     return [p for p in words if p.lower() == p.lower()[::-1]]
+     #return [p for p in words if p.lower() == p.lower()[::-1]]
+     palindromes = [] #our list of palindromes
+     # Iterate through each word in the list
+
+     #1. lowercase the word
+     for p in words:
+         cleaned_word = p.lower()
+     #2. Spaces
+         cleaned_word = ''.join(filter(str.isalnum, cleaned_word))
+     #3. Check if the cleaned word is a palindrome
+         if cleaned_word == cleaned_word[::-1] and cleaned_word != '':
+             palindromes.append(p)
+
+     return palindromes
 
 words_list = [
       "civic",
       "kayak",
-      "racecar",
+      "racecar?",
       "cookie",
       "madam",
       "cartoon",
       "noon",
       "Common",
      "deed",
-     "No lemon, no melon"
+     "No lemon, no melon!"
 ]
 
 print(choose_palindromes(words_list))
